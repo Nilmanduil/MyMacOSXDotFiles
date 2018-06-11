0a69cf30eda6e406a6cf0c39b9a1e02d4efe9cdc
Thibault Goudouneix <Nilmanduil@users.noreply.github.com>
2018-06-11 16:51:34 +0200

    Add system defaults

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
+# Mac defaults
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+# Show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+# Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
 # Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Brew formulae
-brew install fortune cowsay libsass
+brew install fortune cowsay libsass coreutils
 
 # NVM + Node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
