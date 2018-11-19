ee121b7a9d2f3aad17289347fa29ce5544e9affa
Thibault Goudouneix <Nilmanduil@users.noreply.github.com>
2018-11-19 15:24:48 +0100

    Useful additions

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+# Ask for the administrator password upfront.
+sudo -v
 
 # Mac defaults
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 # Show hidden files
 defaults write com.apple.Finder AppleShowAllFiles true
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
@@ -14,12 +19,30 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
 
 # Brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Brew formulae
-brew install fortune cowsay libsass coreutils cmatrix wget m-cli fd shellcheck osquery
+brew update
+brew upgrade --all
+brew install android-sdk fortune cowsay libsass coreutils moreutils findutils cmatrix wget m-cli fd shellcheck osquery
+brew cleanup
 
 # NVM + Node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
@@ -39,3 +62,6 @@ mkdir ~/super/jars
 mkdir ~/super/dists
 mkdir ~/super/results
 mkdir ~/super/tested
+
+sudo softwareupdate -ir
+xcode-select --install
